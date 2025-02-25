c47d88036e675d2fb90aaec889f479e36d0e83e9
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-25 20:31:40 -0300

    :tada: first commit

---
+++ src/main/java/com/dio/bootcamp_dio_java/BootcampDioJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bootcamp_dio_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDioJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bootcamp_dio_java/BootcampDioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bootcamp_dio_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
