756388c9095f7e207fbaac2aa709a4fd6d21e178
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-25 20:41:18 -0300

    :sparkles: initialize Spring Boot application

--- src/main/java/com/dio/bootcamp_dio_java/Application.java
+++ src/main/java/com/dio/bootcamp_dio_java/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootcampDioJavaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootcampDioJavaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
