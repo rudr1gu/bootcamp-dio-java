a004ade199ae3f3e67dc7101120265d3122eb048
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-26 10:25:49 -0300

    :bug: fix: rename limit field to additional_limit in Account and Card entities for clarity

--- src/main/java/com/dio/bootcamp_dio_java/domain/model/Account.java
+++ src/main/java/com/dio/bootcamp_dio_java/domain/model/Account.java
@@ -29,6 +29,6 @@ public class Account {
     @Column(precision = 2, scale = 13)
     private BigDecimal balance;
 
-    @Column(precision = 2, scale = 13)
+    @Column(name = "additional_limit", precision = 2, scale = 13)
     private BigDecimal limit;
 }
