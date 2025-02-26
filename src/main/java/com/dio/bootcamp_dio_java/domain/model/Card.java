8f704ee485fddfcc146b2b8d2c72581260311772
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-26 10:15:33 -0300

    :sparkles: feat: add unique constraint to Card number and precision to limit field

--- src/main/java/com/dio/bootcamp_dio_java/domain/model/Card.java
+++ src/main/java/com/dio/bootcamp_dio_java/domain/model/Card.java
@@ -2,22 +2,29 @@ package com.dio.bootcamp_dio_java.domain.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tb_card")
 public class Card {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String number;
+
+    @Column(precision = 2, scale = 13)
     private BigDecimal limit;
 
 }
