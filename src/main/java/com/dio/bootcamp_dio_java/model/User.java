a5b996643c312bfbf4a97a3f5df014427a013979
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-25 21:42:21 -0300

    :sparkles: feat: add User entity with id and name fields

---
+++ src/main/java/com/dio/bootcamp_dio_java/model/User.java
@@ -0,0 +1,22 @@
+package com.dio.bootcamp_dio_java.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    private String name;
+}
