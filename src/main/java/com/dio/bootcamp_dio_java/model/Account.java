f9e521a9a024e25f59d83a693aa9d00a2e713a28
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-25 21:47:39 -0300

    :sparkles: feat: add Account entity with fields for number, agency, balance, and limit

---
+++ src/main/java/com/dio/bootcamp_dio_java/model/Account.java
@@ -0,0 +1,28 @@
+package com.dio.bootcamp_dio_java.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_account")
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private long number;
+
+    private String agency;
+
+    private double balance;
+
+    private double limit;
+}
