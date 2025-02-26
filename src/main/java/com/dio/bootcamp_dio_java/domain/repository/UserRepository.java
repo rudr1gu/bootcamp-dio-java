e36cbdc711a03c96118364236b19fc97765e9e94
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-26 10:31:07 -0300

    :sparkles: feat: add UserRepository interface for User entity persistence

---
+++ src/main/java/com/dio/bootcamp_dio_java/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dio.bootcamp_dio_java.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dio.bootcamp_dio_java.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
