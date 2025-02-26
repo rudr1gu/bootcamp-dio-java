7a4f61b43794092f22efecb2754c909300636f60
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-26 10:09:51 -0300

    :sparkles: feat: enhance User entity with relationships to Account, Card, Feature, and News

--- src/main/java/com/dio/bootcamp_dio_java/domain/model/User.java
+++ src/main/java/com/dio/bootcamp_dio_java/domain/model/User.java
@@ -2,10 +2,14 @@ package com.dio.bootcamp_dio_java.domain.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,14 +22,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
 
+    @OneToOne(cascade = CascadeType.ALL)
     private Account account;
 
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Feature> features;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
 }
