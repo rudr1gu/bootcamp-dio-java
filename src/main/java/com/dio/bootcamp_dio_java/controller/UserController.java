bcf5e7e4d8c4074fdb08e1b91c3aa1a7dd33ab01
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-03-03 14:49:00 -0300

    :sparkles: feat: add create user endpoint to UserController

--- src/main/java/com/dio/bootcamp_dio_java/controller/UserController.java
+++ src/main/java/com/dio/bootcamp_dio_java/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.dio.bootcamp_dio_java.controller;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dio.bootcamp_dio_java.domain.model.User;
 import com.dio.bootcamp_dio_java.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +29,14 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var user = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(location).body(user);
     }
 
 }
